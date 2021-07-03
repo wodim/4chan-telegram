5e63afc87fa5f862ffe4689d244e046c2fe20fe0
wodim <wodim@vortigaunt.net>
2021-07-03 02:17:13 +0200

    convert webms to mp4s

--- bot.py
+++ bot.py
@@ -1,7 +1,9 @@
 import datetime
 import logging
+import os
 import random
 import re
+import subprocess
 
 import telegram
 from telegram import Update
@@ -14,13 +16,29 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(message)s', level=logging
 logger = logging.getLogger(__name__)
 
 DEFAULT_BOARDS = 'r9k b int'.split(' ')
+FFMPEG_CMD = 'ffmpeg -hide_banner -i \'{source}\' -preset veryfast \'{dest}\''
+
 _4c = _4chan()
 rx_greentext = re.compile(r'^(\\>.*)$', re.MULTILINE)
 
 
-def e(text):
+def _e(text):
     """escapes text with markdown v2 syntax"""
     return telegram.utils.helpers.escape_markdown(text, 2)
+
+
+def _webm_convert(file: str) -> str:
+    """converts a webm to a mp4 file"""
+    new_file = file + '.mp4'
+    if os.path.exists(new_file):
+        return new_file
+
+    logger.info('converting %s to %s', file, new_file)
+    subprocess.call(FFMPEG_CMD.format(source=file, dest=new_file), shell=True)
+    if not os.path.exists(new_file):
+        raise RuntimeError("for some reason, %s wasn't created" % new_file)
+
+    return new_file
 
 
 def post_thread(chat_id: int, context: CallbackContext, args: list = None) -> None:
@@ -36,20 +54,23 @@ def post_thread(chat_id: int, context: CallbackContext, args: list = None) -> No
 
     text = ''
 
-    # text = '_%s_\n\n' % e(thread['image_info'])
+    # text = '_%s_\n\n' % _e(thread['image_info'])
 
     thread_text = thread['text']
     if len(thread_text) > 3000:
         thread_text = thread_text + '…'
-    thread_text = rx_greentext.sub(r'_\1_', e(thread_text))
+    thread_text = rx_greentext.sub(r'_\1_', _e(thread_text))
     if thread['subject'] and thread_text:
-        text += '*%s*\n%s' % (e(thread['subject']), thread_text,)
+        text += '*%s*\n%s' % (_e(thread['subject']), thread_text,)
     elif thread['subject']:
-        text = e(thread['subject'])
+        text = _e(thread['subject'])
     else:
         text = thread_text
 
-    text += '\n\n' + e(thread['url'])
+    text += '\n\n' + _e(thread['url'])
+
+    if thread['image_url'].endswith('.webm'):
+        thread['image_file'] = _webm_convert(thread['image_file'])
 
     if thread['image_url'].endswith('.gif') or thread['image_url'].endswith('.webm'):
         fun = context.bot.send_document
